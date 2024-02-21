3ad54ee82fa00f514b6a102b31bd9b3ce3e01460
Harihs14 <149752809+Harihs14@users.noreply.github.com>
2024-02-21 15:58:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+// script.js
+
+function searchWeather() {
+    const locationInput = document.getElementById('locationInputOverlay');
+    const cityName = locationInput.value;
+
+    // Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+    const apiKey = '191a1864be336917d1d37dcce2d3f0d9';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeatherInfo(data, cityName);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}
+
+function displayWeatherInfo(weatherData, cityName) {
+    const weatherCity = document.getElementById('weatherCity');
+    const weatherDescription = document.getElementById('weatherDescription');
+    const weatherInfo = document.getElementById('weatherInfo');
+
+    if (weatherData.cod === '404') {
+        // Handle case when city is not found
+        weatherCity.textContent = 'City not found';
+        weatherDescription.textContent = '';
+        weatherInfo.textContent = '';
+    } else {
+        const description = weatherData.weather[0].description;
+        const temperatureKelvin = weatherData.main.temp; // Temperature in Kelvin
+        const temperatureCelsius = temperatureKelvin - 273.15; // Convert Kelvin to Celsius
+        const humidity = weatherData.main.humidity;
+
+        weatherCity.textContent = `Weather in ${cityName}`;
+        weatherDescription.textContent = `Current conditions: ${description}`;
+        weatherInfo.innerHTML = `Temperature: ${temperatureCelsius.toFixed(2)}°C<br>Humidity: ${humidity}%`;
+
+        // Show pop-up (modal)
+        showPopup(cityName);
+    }
+}
+function showPopup(cityName) {
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
+    
+    const closeBtn = document.createElement('span');
+    closeBtn.classList.add('close-btn');
+    closeBtn.innerHTML = '&times;';
+    closeBtn.addEventListener('click', closePopup);
+    
+    const popupContent = document.createElement('div');
+    popupContent.classList.add('popup-content');
+    
+    const weatherInfo = document.getElementById('weatherInfo').innerHTML;
+    popupContent.innerHTML = `<h2>${cityName}</h2>${weatherInfo}`;
+    
+    popup.appendChild(closeBtn);
+    popup.appendChild(popupContent);
+    
+    document.body.appendChild(popup);
+}
+
+function closePopup() {
+    const popup = document.querySelector('.popup');
+    if (popup) {
+        popup.remove();
+    }
+}
